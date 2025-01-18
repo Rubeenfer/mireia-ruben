47eb591e6605d4c6da1720b2c4b467274ef78d0d
Rubén Fernández <rubeen.fer@gmail.com>
2025-01-18 16:59:21 +0100

    Actualizar estilos y mejorar overlays en componentes Location, OurStory y RSVP

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,52 +1,48 @@
 /** @type {import('tailwindcss').Config} */
 
-// OVERLAYS:
-const bottomLeft =
-  "linear-gradient(-1deg, rgba(208, 212, 194, 1) 25%, rgba(208, 212, 194, 0.5) 50%, rgba(208, 212, 194, 0) 75%)";
-const bottomRight =
-  "linear-gradient(1deg, rgba(208, 212, 194, 1) 0%, rgba(208, 212, 194, 0.5) 25%, rgba(125, 152, 133, 0) 100%)";
-const darkBottomLeft =
-  "linear-gradient(-1deg, rgba(28, 57, 79, 1) 0%, rgba(28, 57, 79, 0.5) 25%, rgba(125, 152, 133, 0) 100%)";
-const darkBottomRight =
-  "linear-gradient(-1deg, rgba(28, 57, 79, 1) 25%, rgba(28, 57, 79, 0.5) 50%, rgba(28, 57, 79, 0) 75%)";
-const darkTopLeft =
-  "linear-gradient(1deg, rgba(125, 152, 133, 0) 0%, rgba(28, 57, 79, 0.5) 75%, rgba(28, 57, 79, 1) 100%)";
-const darkTopRight =
-  "linear-gradient(-1deg,rgba(28, 57, 79, 0) 25%, rgba(28, 57, 79, 0.5) 50%, rgba(28, 57, 79, 1) 75%)";
-const topLeft =
-  "linear-gradient(-1deg, rgba(208, 212, 194, 0) 25%, rgba(208, 212, 194, 0.5) 50%, rgba(208, 212, 194, 1) 75%)";
-const topRight =
-  "linear-gradient(-1deg, rgba(125, 152, 133, 0) 0%, rgba(208, 212, 194, 0.5) 75%, rgba(208, 212, 194, 1) 100%)";
+// COLORS:
+const darkBlue = "rgba(16, 24, 39, 1)";
+const darkblue80 = "rgba(16, 24, 39, 0.8)";
+const darkGreen = "rgba(125, 152, 133, 1)";
+const darkGreen80 = "rgba(125, 152, 133, 0.8)";
+const lightBlue = "rgba(28, 57, 79, 1)";
+const lightBlue80 = "rgba(28, 57, 79, 0.8)";
+const lightGreen = "rgba(208, 212, 194, 1)";
+const lightGreen80 = "rgba(208, 212, 194, 0.8)";
+const transparent = "rgb(0, 0, 0, 0)";
 
-// BACKGROUND-COLORS:
-const olive =
-  "linear-gradient( 95deg, rgba(208, 212, 194, 1) 0%, rgba(208, 212, 194, 1) 40%, rgba(208, 212, 194, 0.8) 60%, rgba(125, 152, 133, 1) 100%)";
-const oliveReverse =
-  "linear-gradient( 95deg, rgba(125, 152, 133, 1) 0%, rgba(208, 212, 194, 0.8) 40%, rgba(208, 212, 194, 1) 60%, rgba(208, 212, 194, 1) 100%)";
+// IMAGES:
+const farineraDay = "/images/farinera_day.jpg";
+const farineraNight = "/images/farinera_night.jpg";
+const ourStoryDay = "/images/our_story.jpg";
+const ourStoryNight = "/images/test2.jpeg";
+const dock = "/images/dock.jpg";
+
+// RADIAL-COLORS:
+const night = `radial-gradient(circle at 0% 100%,  ${darkBlue} 5vw, ${lightBlue} 50vw, ${lightBlue} 100vw)`;
+const nightReverse = `radial-gradient(circle at 0% 0%,  ${darkBlue} 5vw, ${lightBlue} 50vw, ${lightBlue} 100vw)`;
+
+const olive = `radial-gradient(circle at 100% 100%, ${darkGreen}  5vw, ${lightGreen} 50vw, ${lightGreen} 100vw)`;
+const oliveReverse = `radial-gradient(circle at 100% 0%, ${darkGreen}  5vw, ${lightGreen} 50vw, ${lightGreen} 100vw)`;
+
+// LINEAR-COLORS:
+const oliveOverlay = `linear-gradient(0deg, ${transparent} 0%, ${lightGreen} 50%)`;
+const oliveOverlayReverse = `linear-gradient(0deg, ${lightGreen} 50%, ${transparent} 100%)`;
+
+const nightOverlay = `linear-gradient(0deg, ${transparent} 0%, ${lightBlue} 50%)`;
+const nightOverlayReverse = `linear-gradient(0deg, ${lightBlue} 50%, ${transparent} 100%)`;
 
 // BACKGROUND-IMAGES:
-const hero =
-  "linear-gradient(0deg, rgb(211, 215, 196, 0), rgb(211, 215, 196, 0.3)), url('/images/farinera_day.jpg')";
-const darkHero =
-  "linear-gradient(0deg, rgb(28, 57, 79, 0), rgb(28, 57, 79, 0.7)), url('/images/farinera_night.jpg')";
+const hero = `linear-gradient(0deg, ${transparent}, ${darkGreen80}), url(${farineraDay})`;
+const darkHero = `linear-gradient(0deg, ${transparent}, ${darkblue80}), url(${farineraNight})`;
 
-const ourStoryMobile =
-  "linear-gradient(0deg, rgba(208, 212, 194, 0.8), rgba(208, 212, 194, 0.8)), url('/images/our_story.jpg')";
-const ourStory =
-  "linear-gradient(95deg, rgba(208, 212, 194, 1) 10%, rgba(208, 212, 194, 0) 20%, rgba(208, 212, 194, 0) 80%, rgba(208, 212, 194, 1) 90%),  url('/images/our_story.jpg')";
-const ourStory2 =
-  "linear-gradient( 95deg, rgba(208, 212, 194, 1) 0%, rgba(208, 212, 194, 1) 40%, rgba(208, 212, 194, 0.8) 60%, rgba(125, 152, 133, 1) 100%)";
-const darkOurStoryMobile =
-  "linear-gradient(0deg, rgba(28, 57, 79, 0.8), rgba(28, 57, 79, 0.8)), url('/images/test2.jpeg')";
-const darkOurStory =
-  "linear-gradient(85deg, rgba(28, 57, 79, 1) 10%, rgba(28, 57, 79, 0) 20%, rgba(28, 57, 79, 0) 80%, rgba(28, 57, 79, 1) 90%), url('/images/test2.jpeg')";
-const darkOurStory2 =
-  "linear-gradient(95deg, rgba(16, 24, 39, 1) 0%, rgba(28, 57, 79, 0.8) 40%, rgba(28, 57, 79, 1) 60%, rgba(28, 57, 79, 1) 100%)";
+const ourStoryMobile = `linear-gradient(0deg, ${lightGreen80}, ${lightGreen80}), url(${ourStoryDay})`;
+const ourStory = `linear-gradient(95deg, ${lightGreen} 10%, ${transparent} 20%, ${transparent} 80%, ${lightGreen} 90%),  url(${ourStoryDay})`;
+const darkOurStoryMobile = `linear-gradient(0deg, ${lightBlue80}, ${lightBlue80}), url(${ourStoryNight})`;
+const darkOurStory = `linear-gradient(85deg, ${lightBlue} 10%, ${transparent} 20%, ${transparent} 80%, ${lightBlue} 90%), url(${ourStoryNight})`;
 
-const schedule =
-  "linear-gradient(0deg, rgb(211, 215, 196, 0), rgb(211, 215, 196, 0.3)), url('/images/dock.jpg')";
-const darkSchedule =
-  "linear-gradient(0deg,  rgb(28, 57, 79, 0), rgb(28, 57, 79, 0.7)), url('/images/dock.jpg')";
+const schedule = `linear-gradient(0deg, ${transparent}, ${darkGreen80}), url(${dock})`;
+const darkSchedule = `linear-gradient(0deg,  ${transparent}, ${darkblue80}), url(${dock})`;
 
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
@@ -61,26 +57,22 @@ module.exports = {
         light: "#d4d8c5",
       },
       backgroundImage: {
-        hero,
         darkHero,
-        ourStoryMobile,
-        ourStory,
-        ourStory2,
-        darkOurStoryMobile,
         darkOurStory,
-        darkOurStory2,
-        schedule,
+        darkOurStoryMobile,
         darkSchedule,
-        bottomLeft,
-        bottomRight,
-        darkBottomLeft,
-        darkBottomRight,
-        darkTopLeft,
-        darkTopRight,
-        topLeft,
-        topRight,
+        hero,
+        night,
+        nightOverlay,
+        nightOverlayReverse,
+        nightReverse,
         olive,
+        oliveOverlay,
+        oliveOverlayReverse,
         oliveReverse,
+        ourStory,
+        ourStoryMobile,
+        schedule,
       },
     },
   },
