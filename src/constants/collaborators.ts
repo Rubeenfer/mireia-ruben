69c69ca6666c17989fddff6296ee54c9e4c11f12
Rubén Fernández <rubeen.fer@gmail.com>
2025-01-23 19:12:53 +0100

    Actualizar estilos en Collaborator y Divider; agregar prefijo internacional a los números de teléfono en colaboradores y mejorar la claridad visual en la sección Proposal.

--- src/constants/collaborators.ts
+++ src/constants/collaborators.ts
@@ -1,43 +1,42 @@
 export const collaborators = [
   {
     name: "Unión Suiza",
-    phone: "934 16 11 11",
+    phone: "+34 934 16 11 11",
     link: "unionsuiza.com",
   },
   {
     name: "Farinera Sant Lluís",
-    phone: "672 14 26 51",
+    phone: "+34 672 14 26 51",
     link: "lafarinerasantlluis.com",
   },
   {
     name: "Criscatering",
-    phone: "972 29 13 48",
+    phone: "+34 972 29 13 48",
     link: "criscatering.com",
   },
   {
     name: "Le Chris Musik",
-    phone: "638 60 65 04",
+    phone: "+34 638 60 65 04",
     link: "www.instagram.com/lechrismusik",
   },
   {
     name: "Sweet dreams",
-    phone: "647 97 30 04",
+    phone: "+34 647 97 30 04",
     link: "www.instagram.com/sweetdreams_productions",
   },
   {
     name: "Estudi de llum",
-    phone: "678 14 12 03",
+    phone: "+34 678 14 12 03",
     link: "estudidellum.com",
   },
   {
     name: "Escuer Atelier",
-    phone: "657 93 60 06",
+    phone: "+34 657 93 60 06",
     link: "escueratelier.com",
   },
-
   {
     name: "The Forest Shoes",
-    phone: "932 17 67 89",
+    phone: "+34 932 17 67 89",
     link: "www.instagram.com/theforestshoes",
   },
 ];
