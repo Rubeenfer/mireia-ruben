7e1f36b17518241483670eae5fce57ae81e2612f
Rubén Fernández <rubeen.fer@gmail.com>
2025-01-26 18:49:36 +0100

    Agregar soporte para variables de entorno y eliminar importación innecesaria en Location.astro

--- astro.config.js
+++ astro.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, envField } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
 export default defineConfig({
   integrations: [tailwind(), react()],
+  env: {
+    schema: {
+      API_KEY: envField.string({ context: "server", access: "secret" }),
+    },
+  },
 });
